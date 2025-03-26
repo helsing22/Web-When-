a3c3fa4927d5107cf1c82a6e8ba46876d08347c3
diandev <dianhuiza@gmail.com>
2025-03-26 16:53:05 -0400

    feat: prepare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://hellsing22.github.io",
+  base: "Web-When-",
   integrations: [mdx(), sitemap()],
 
   vite: {
@@ -15,4 +16,6 @@ export default defineConfig({
       tailwindcss(),
     ],
   },
+
+  
 });
