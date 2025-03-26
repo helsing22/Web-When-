b61775fc5b23c38f2cc0b062f42a6a77f1889bf1
Hels <128660296+helsing22@users.noreply.github.com>
2025-03-26 14:20:08 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://helsing22.github.io',
+  base: 'Web-When-',
   integrations: [mdx(), sitemap()],
 
   vite: {
