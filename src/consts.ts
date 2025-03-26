315b1514029b45025e096410c548071a06ec69a6
diandev <dianhuiza@gmail.com>
2025-03-26 12:54:54 -0400

    feat: add fonts and tailwindcss

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'When Game';
+export const SITE_TITLE = 'When';
 export const SITE_DESCRIPTION = 'When Game Landing Page';
