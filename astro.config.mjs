2d19073a0591135d3cc2d1eadf92e1482da1f950
diandev <dianhuiza@gmail.com>
2025-03-27 18:53:50 -0400

    fix:font import warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
 
   vite: {
+    assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
     plugins: [
       tailwindcss(),
     ],
