7dfbbe474872dfd9396578a2470c03bedb4bea7a
diandev <dianhuiza@gmail.com>
2025-03-28 18:56:27 -0400

    fix: responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hellsing22.github.io",
   base: "Web-When-",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
 
   vite: {
     assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
